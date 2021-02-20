fcac5828e1abafa3afc4c4a2c0d8503a06e73f52
pinjopaul <79368906+pinjopaul@users.noreply.github.com>
2021-02-20 14:07:12 -0500

    Add files via upload

---
+++ src/test/java/com/java/openshift/demo/SpringOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.openshift.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

26a4dbbc51d49bc583da8de0386ee5dc8fa6334f
pinjopaul <79368906+pinjopaul@users.noreply.github.com>
2021-02-23 12:32:23 -0500

    Update SpringOpenshiftApplication.java

--- src/main/java/com/java/openshift/demo/SpringOpenshiftApplication.java
+++ src/main/java/com/java/openshift/demo/SpringOpenshiftApplication.java
@@ -27,7 +27,7 @@ public class SpringOpenshiftApplication {
 	
 	@GetMapping("/")
 	public String welcome() {
-		return "Welcome ..." ;		
+		return "Welcome ..." +bot;		
 	}
 
 	@GetMapping("/{input}")
